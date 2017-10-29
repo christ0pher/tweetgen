4ba5d5c5410ffae544af9e0bc7a892426f5bcbc8
Christopher Lege <christopher@levire.com>
2017-10-29 19:08:59 +0100

    Added tweet gen which trains a neural network with word predictions. 3 words input one word prediction. example files available.

---
+++ tweetgen.py
@@ -0,0 +1,86 @@
+import pandas as pd
+from keras import Input
+from keras.engine import Model
+from keras.layers import Lambda, Dense, Concatenate
+import keras.backend as K
+from keras.optimizers import SGD
+
+__author__ = 'christopher@levire.com'
+
+USER = "janboehm"
+TRAIN_CSV = "./train_data/"+USER+".csv"
+TRAIN_META_CSV = "./train_data/"+USER+"_meta.csv"
+TRAIN_VOCAB_CSV = "./train_data/"+USER+"_meta.csv"
+MODEL_FILE = "./models/" + USER + ".model"
+
+if __name__ == "__main__":
+
+    meta_data = pd.read_csv(
+        TRAIN_META_CSV
+    )
+
+    trainings_set = pd.read_csv(TRAIN_CSV)
+
+    vocab_list = pd.read_csv(TRAIN_VOCAB_CSV)
+
+    vec_len = meta_data["word_vec_len"][0]
+
+    print(vec_len)
+
+    w1_input = Input(shape=(1,), dtype="int32")
+    w2_input = Input(shape=(1,), dtype="int32")
+    w3_input = Input(shape=(1,), dtype="int32")
+    target_input = Input(shape=(1,), dtype="int32")
+
+    w1_one_hot = Lambda(
+        K.one_hot,
+        arguments={"num_classes": vec_len},
+        output_shape=(1, vec_len),
+        name="w1_one_hot"
+    )(w1_input)
+
+    w2_one_hot = Lambda(
+        K.one_hot,
+        arguments={"num_classes": vec_len},
+        output_shape=(1, vec_len),
+        name="w2_one_hot"
+    )(w2_input)
+
+    w3_one_hot = Lambda(
+        K.one_hot,
+        arguments={"num_classes": vec_len},
+        output_shape=(1, vec_len),
+        name="w3_one_hot"
+    )(w3_input)
+
+    target_one_hot = Lambda(
+        K.one_hot,
+        arguments={"num_classes": vec_len},
+        output_shape=(1, vec_len),
+        name="target_one_hot"
+    )(target_input)
+
+    word_embedding = Dense(128, activation="linear")
+
+    w1_embedding = word_embedding(w1_one_hot)
+    w2_embedding = word_embedding(w2_one_hot)
+    w3_embedding = word_embedding(w3_one_hot)
+
+    embedding = Concatenate()([w1_embedding, w2_embedding, w3_embedding])
+
+    hidden = Dense(256, activation="sigmoid")(embedding)
+    prediction_layer = Dense(vec_len, activation="softmax")(hidden)
+
+    model = Model(inputs=[w1_input, w2_input, w3_input, target_input], outputs=prediction_layer)
+
+    sgd = SGD(lr=.01)
+
+    model.compile(optimizer=sgd, loss="categorical_crossentropy", metrics=["categorical_accuracy"], target_tensors=[target_one_hot])
+
+    print(model.summary())
+
+    h = model.fit(x=[trainings_set["w1"], trainings_set["w2"], trainings_set["w3"], trainings_set["target"]], verbose=2, epochs=500)
+
+    model.save(MODEL_FILE)
+
+    print(h)
