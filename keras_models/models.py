f81db1bb9626ee10cd3d95b0fe31a72344ebe2a8
Christopher Lege <christopher@levire.com>
2017-11-05 00:20:33 +0100

    A better model bringing random tweets to the world

--- keras_models/models.py
+++ keras_models/models.py
@@ -1,6 +1,6 @@
 from keras import Input
 from keras.engine import Model
-from keras.layers import Lambda, Dense, Concatenate, K
+from keras.layers import Lambda, Dense, Concatenate, K, LSTM
 
 __author__ = 'christopher@levire.com'
 
@@ -53,14 +53,45 @@ def get_tweet_model(vec_len):
         output_shape=(1, vec_len),
         name="target_one_hot"
     )(target_input)
+    """
     word_embedding = Dense(1024, activation="linear")
     w1_embedding = word_embedding(w1_one_hot)
     w2_embedding = word_embedding(w2_one_hot)
     w3_embedding = word_embedding(w3_one_hot)
+    
     embedding = Concatenate()([w1_embedding, w2_embedding, w3_embedding])
+    """
+    w1_w2v_trained = Dense(100, activation="linear", name="w1_w2v")
+    w1_w2v_trained.trainable = False
+    w1_w2v_trained = w1_w2v_trained(w1_one_hot)
+
+    w2_w2v_trained = Dense(100, activation="linear", name="w2_w2v")
+    w2_w2v_trained.trainable = False
+    w2_w2v_trained = w2_w2v_trained(w2_one_hot)
+
+    w3_w2v_trained = Dense(100, activation="linear", name="w3_w2v")
+    w3_w2v_trained.trainable = False
+    w3_w2v_trained = w3_w2v_trained(w3_one_hot)
+    """
+    mem_1 = LSTM(64)(w1_w2v_trained)
+    mem_2 = LSTM(64)(w2_w2v_trained)
+    mem_3 = LSTM(64)(w3_w2v_trained)
+
+    memento_embedding = Concatenate()([mem_1, mem_2, mem_3])
+    """
+    w1_learn = Dense(256, activation="linear")(w1_w2v_trained)
+    w2_learn = Dense(256, activation="linear")(w2_w2v_trained)
+    w3_learn = Dense(256, activation="linear")(w3_w2v_trained)
+
+    combined_learner = Dense(1500, activation="sigmoid")
+    w1 = combined_learner(w1_learn)
+    w2 = combined_learner(w2_learn)
+    w3 = combined_learner(w3_learn)
+
+    embedding = Concatenate()([w1, w2, w3])
+
     hidden = Dense(512, activation="sigmoid")(embedding)
-    hidden2 = Dense(128, activation="hard_sigmoid")(hidden)
-    prediction_layer = Dense(vec_len, activation="softmax")(hidden2)
+    prediction_layer = Dense(vec_len, activation="softmax")(hidden)
     model = Model(inputs=[w1_input, w2_input, w3_input, target_input], outputs=prediction_layer)
     model.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=["categorical_accuracy"],
                   target_tensors=[target_one_hot])
