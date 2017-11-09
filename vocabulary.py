b7f33bfe0a284d4720e1c59033e81d0dd8001a89
Daniel Kiedrowski <daniel@levire.com>
2017-11-09 17:16:48 +0100

    Added creating a vocabulary list from a raw text file.

--- vocabulary.py
+++ vocabulary.py
@@ -1,6 +1,37 @@
 import pandas as pd
 
 
+def create(filename, word_used_at_least_num_times=None, encoding=None):
+    if word_used_at_least_num_times is None:
+        word_used_at_least_num_times = 2
+
+    if encoding is None:
+        encoding = "utf8"
+
+    words = set()
+    frequencies = {}
+
+    with open(filename, encoding=encoding) as input_file:
+        for line in input_file:
+            for word in line.split():
+                word = word.lower()
+                if word not in frequencies:
+                    frequencies[word] = 1
+                else:
+                    frequencies[word] += 1
+
+                if frequencies[word] >= word_used_at_least_num_times:
+                    words.add(word)
+
+    items = []
+    index = 0
+    for word in words:
+        items.append((index,  [index, word]))
+        index += 1
+
+    return pd.DataFrame.from_items(items, columns=["index", "word"], orient="index")
+
+
 def load(filename):
     return pd.read_csv(filename)
 
