df3ffede2f8c66d2ce2618b20c0ccb5d88d30eaf
Christopher Lege <christopher@levire.com>
2017-11-09 12:03:53 +0100

    fixed word2vec

--- word2vec.py
+++ word2vec.py
@@ -1,15 +1,23 @@
+import numpy
 import pandas as pd
 from keras import Input, callbacks
 from keras.engine import Model
 from keras.layers import Lambda, K, Dense
 from keras.metrics import top_k_categorical_accuracy
 from os.path import exists
+import json
+from sklearn.cluster import KMeans
+
+from helper_algorithms.words_kmeans import get_kmeans
+from keras_models.models import get_w2v_model
+
+__author__ = 'christopher@levire.com'
 
 USER = "realDonaldTrump"
 TRAIN_META_CSV = "./train_data/"+USER+"_meta.csv"
 TRAIN_VOCAB_CSV = "./train_data/"+USER+"_vocab.csv"
 WORD2VEC_TRAIN = "./train_data/"+USER+"_w2v.csv"
-WORD2VEC_TRAIN_MODEL = "D:\\models\\"+USER+"_w2v.model"
+WORD2VEC_TRAIN_MODEL = "./models/"+USER+"_w2v.model"
 RAW_TEXT = "./raw_data/"+USER+".txt"
 
 
@@ -17,9 +25,9 @@ def top_3_accuracy(y_true, y_pred):
     return top_k_categorical_accuracy(y_true, y_pred, k=3)
 
 
-def word_to_int(word, vocab_list):
-    if len(vocab_list[vocab_list["word"] == word]["index"]) == 1:
-        return vocab_list[vocab_list["word"] == word]["index"].values[0]
+def wtoint(w, vocab):
+    if len(vocab[vocab["word"] == w]["index"]) == 1:
+        return vocab[vocab["word"] == w]["index"].values[0]
     return None
 
 
@@ -38,13 +46,13 @@ if __name__ == "__main__":
                 for line in input_file:
                     words = line.split()
                     for i in range(0, len(words)):
-                        w = word_to_int(words[i], vocab_list)
+                        w = wtoint(words[i], vocab_list)
                         if w is None:
                             continue
                         left_words = words[max(0, i-4):max(0, i-1)]
                         right_words = words[min(i+1, len(words)-1):min(i+4, len(words)-1)]
                         for context_word in left_words+right_words:
-                            cw = word_to_int(context_word, vocab_list)
+                            cw = wtoint(context_word, vocab_list)
                             if w is not None and cw is not None:
                                 w2vf.write(str(w)+","+str(cw)+"\n")
 
@@ -52,51 +60,66 @@ if __name__ == "__main__":
 
     vec_len = meta_data["word_vec_len"][0]
 
-    w1 = Input(shape=(1,), dtype="int32")
-    w2 = Input(shape=(1,), dtype="int32")
+    if not exists(WORD2VEC_TRAIN_MODEL):
+        w1 = Input(shape=(1,), dtype="int32")
+        w2 = Input(shape=(1,), dtype="int32")
 
-    w1_input_hot = Lambda(
-        K.one_hot,
-        arguments={"num_classes": vec_len},
-        output_shape=(1, vec_len),
-        name="w1_input_hot"
-    )(w1)
+        w1_input_hot = Lambda(
+            K.one_hot,
+            arguments={"num_classes": vec_len},
+            output_shape=(1, vec_len),
+            name="w1_input_hot"
+        )(w1)
 
-    w2_input_hot = Lambda(
-        K.one_hot,
-        arguments={"num_classes": vec_len},
-        output_shape=(1, vec_len),
-        name="w2_input_hot"
-    )(w2)
+        w2_input_hot = Lambda(
+            K.one_hot,
+            arguments={"num_classes": vec_len},
+            output_shape=(1, vec_len),
+            name="w2_input_hot"
+        )(w2)
 
-    encoding_layer = Dense(100, activation="linear")(w1_input_hot)
-    prediction = Dense(vec_len, activation="softmax")(encoding_layer)
+        encoding_layer = Dense(100, activation="linear", name="encoding_layer")(w1_input_hot)
 
-    model = Model(inputs=[w1, w2], outputs=prediction)
+        prediction = Dense(vec_len, activation="softmax")(encoding_layer)
 
-    model.compile(optimizer="sgd", loss="categorical_crossentropy", metrics=["categorical_accuracy", 'binary_accuracy'],
-                  target_tensors=[w2_input_hot])
+        model = Model(inputs=[w1, w2], outputs=prediction)
 
-    h = model.fit(
-        x=[trainings_set["word"], trainings_set["context_word"]], verbose=1,
-        epochs=5000,
-        batch_size=1000,
-        callbacks=[
-            callbacks.TensorBoard(
-                log_dir='./Graph',
-                histogram_freq=0,
-                write_graph=True,
-                write_images=True
-            ),
-            callbacks.ModelCheckpoint(
-                WORD2VEC_TRAIN_MODEL,
-                monitor='loss',
-                verbose=0,
-                save_best_only=True,
-                save_weights_only=True,
-                mode='auto',
-                period=5
-            ),
-            # callbacks.EarlyStopping(monitor='loss', patience=2, verbose=0, mode='auto', min_delta=0.01)
-        ]
-    )
+        model.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=["categorical_accuracy", 'binary_accuracy'],
+                      target_tensors=[w2_input_hot])
+
+        h = model.fit(x=[trainings_set["word"], trainings_set["context_word"]], verbose=1,
+                      epochs=50, batch_size=1000,
+                      callbacks=[
+                          callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True),
+                          callbacks.ModelCheckpoint(WORD2VEC_TRAIN_MODEL, monitor='loss', verbose=0, save_best_only=True,save_weights_only=True, mode='auto', period=5),
+                          #callbacks.EarlyStopping(monitor='loss', patience=2, verbose=0, mode='auto', min_delta=0.01)
+                      ]
+                      )
+
+    ########################################################################################
+
+    loaded_model = get_w2v_model(vec_len)
+    loaded_model.load_weights(WORD2VEC_TRAIN_MODEL, by_name=True)
+
+    p = loaded_model.predict(x=numpy.array([vocab_list[vocab_list["word"] == "great"]["index"].values[0]]))
+
+    distances = []
+
+    for word in vocab_list["word"]:
+        p_w = loaded_model.predict(x=numpy.array([vocab_list[vocab_list["word"] == word]["index"].values[0]]))
+        dist = numpy.linalg.norm(p.flatten().reshape((100,1)) - p_w.flatten().reshape((100,1)))
+        #print("Obamacare -> "+word+" distance: "+str(dist))
+        distances.append(("great", word, dist))
+    sorted_distances = sorted(distances, key=lambda w: w[2])
+    print(sorted_distances[:20])
+
+    ########################################################################################
+
+    word_centers, _ = get_kmeans(vocab_list, loaded_model)
+    print(json.dumps(word_centers, indent=4, sort_keys=True))
+
+
+
+
+
+
