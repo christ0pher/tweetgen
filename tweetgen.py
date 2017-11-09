3f415d011c637d2e24cc47a8a91b9b6d24125ec3
Christopher Lege <christopher@levire.com>
2017-11-09 11:56:35 +0100

    Fixed the history

--- tweetgen.py
+++ tweetgen.py
@@ -1,14 +1,17 @@
 import pandas as pd
-from keras import Input
-from keras.engine import Model
-from keras.layers import Lambda, Dense, Concatenate
-import keras.backend as K
+from keras import callbacks
 
-USER = "khloekardashian"
+from keras_models.models import get_tweet_model, get_w2v_model
+
+__author__ = 'christopher@levire.com'
+
+USER = "realDonaldTrump"
 TRAIN_CSV = "./train_data/"+USER+".csv"
 TRAIN_META_CSV = "./train_data/"+USER+"_meta.csv"
 TRAIN_VOCAB_CSV = "./train_data/"+USER+"_vocab.csv"
 MODEL_FILE = "./models/" + USER + ".model"
+WORD2VEC_TRAIN_MODEL = "./models/"+USER+"_w2v.model"
+
 
 if __name__ == "__main__":
 
@@ -17,6 +20,7 @@ if __name__ == "__main__":
     )
 
     trainings_set = pd.read_csv(TRAIN_CSV)
+    trainings_set = trainings_set.sample(frac=1).reset_index(drop=True) #shuffle trainingsdata
 
     vocab_list = pd.read_csv(TRAIN_VOCAB_CSV)
 
@@ -24,73 +28,25 @@ if __name__ == "__main__":
 
     print(vec_len)
 
-    w1_input = Input(shape=(1,), dtype="int32")
-    w2_input = Input(shape=(1,), dtype="int32")
-    w3_input = Input(shape=(1,), dtype="int32")
-    target_input = Input(shape=(1,), dtype="int32")
+    w2v_model = get_w2v_model(vec_len)
+    w2v_model.load_weights(WORD2VEC_TRAIN_MODEL, by_name=True)
 
-    w1_one_hot = Lambda(
-        K.one_hot,
-        arguments={"num_classes": vec_len},
-        output_shape=(1, vec_len),
-        name="w1_one_hot"
-    )(w1_input)
+    w2v_weights = w2v_model.get_layer("encoding_layer").get_weights()
 
-    w2_one_hot = Lambda(
-        K.one_hot,
-        arguments={"num_classes": vec_len},
-        output_shape=(1, vec_len),
-        name="w2_one_hot"
-    )(w2_input)
+    model = get_tweet_model(vec_len)
 
-    w3_one_hot = Lambda(
-        K.one_hot,
-        arguments={"num_classes": vec_len},
-        output_shape=(1, vec_len),
-        name="w3_one_hot"
-    )(w3_input)
-
-    target_one_hot = Lambda(
-        K.one_hot,
-        arguments={"num_classes": vec_len},
-        output_shape=(1, vec_len),
-        name="target_one_hot"
-    )(target_input)
-
-    word_embedding = Dense(1024, activation="linear")
-
-    w1_embedding = word_embedding(w1_one_hot)
-    w2_embedding = word_embedding(w2_one_hot)
-    w3_embedding = word_embedding(w3_one_hot)
-
-    embedding = Concatenate()([w1_embedding, w2_embedding, w3_embedding])
-
-    hidden = Dense(512, activation="sigmoid")(embedding)
-
-    hidden2 = Dense(128, activation="hard_sigmoid")(hidden)
-    prediction_layer = Dense(vec_len, activation="softmax")(hidden2)
-
-    model = Model(inputs=[w1_input, w2_input, w3_input, target_input], outputs=prediction_layer)
-
-    model.compile(
-        optimizer="rmsprop",
-        loss="categorical_crossentropy",
-        metrics=["categorical_accuracy"],
-        target_tensors=[target_one_hot]
-    )
+    model.get_layer("w1_w2v").set_weights(w2v_weights)
+    model.get_layer("w2_w2v").set_weights(w2v_weights)
+    model.get_layer("w3_w2v").set_weights(w2v_weights)
 
     print(model.summary())
 
-    h = model.fit(
-        x=[trainings_set["w1"],
-           trainings_set["w2"],
-           trainings_set["w3"],
-           trainings_set["target"]
-           ],
-        verbose=1,
-        epochs=200,
-        batch_size=500
-    )
+    h = model.fit(x=[trainings_set["w1"], trainings_set["w2"], trainings_set["w3"], trainings_set["target"]],
+                  verbose=1, epochs=1000, batch_size=500, callbacks=[
+                          callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True),
+                          callbacks.ModelCheckpoint(MODEL_FILE, monitor='loss', verbose=0, save_best_only=True,save_weights_only=False, mode='auto', period=1),
+                          callbacks.EarlyStopping(monitor='loss', patience=5, verbose=0, mode='auto', min_delta=0.01)
+                      ])
 
     model.save(MODEL_FILE)
 
