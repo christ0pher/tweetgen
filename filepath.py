268fd742467dbd0970720ee2095fc9c74fdbac02
Daniel Kiedrowski <daniel@levire.com>
2017-11-15 15:43:08 +0100

    Added filepaths for skip gram trainingssets and saved w2v models.

--- filepath.py
+++ filepath.py
@@ -6,8 +6,16 @@ def for_vocabulary_file(user):
     return for_trainings_set_file(user, "vocab")
 
 
+def for_skip_gram_file(user):
+    return for_trainings_set_file(user, "skipgram")
+
+
 def for_trainings_set_file(user, suffix=None):
     if suffix is None:
         return "./train_data/" + user + ".csv"
     else:
         return "./train_data/" + user + "_" + suffix + ".csv"
+
+
+def for_w2v_model(user):
+    return "./models/"+user+"_w2v.model"
